10f55869b9e8a7d9308fa2a1908c992d641cb7ab
Robby Novianto <heyitsrobby99@gmail.com>
2023-01-22 15:00:26 +0700

    fix deploy

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => (
                 <img 
                 src="/stamp.png" 
                 alt="stamp"
-                className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] objec' 
+                className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain' 
                 />
               </div>
              </a>
