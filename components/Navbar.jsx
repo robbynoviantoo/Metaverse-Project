eaebd1eff62c7138c880d0c1b12f87541df194e6
Robby Novianto <heyitsrobby99@gmail.com>
2023-01-22 14:50:24 +0700

    as

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,10 +11,7 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    {/* Gradient Color */}
     <div className='absolute w-[50%] inset-0 gradient-01' />
-    {/*  */}
-
 
       <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
         <img 
