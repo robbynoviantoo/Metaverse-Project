1ace67139dc824fe2919aa71e972eacb9618b874
robbynoviantoo <heyitsrobby99@gmail.com>
2023-01-23 00:08:00 +0700

    ExploreCard Finish

--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
 import Navbar from './Navbar';
-import { TypingText } from './CustomTexts';
+import { TypingText, TitleText } from './CustomTexts';
 import ExploreCard from './ExploreCard';
 import StartSteps from './StartSteps';
 import NewFeatures from './NewFeatures';
@@ -8,6 +8,7 @@ import Footer from './Footer';
 
 export {
   Navbar,
+  TitleText,
   TypingText,
   StartSteps,
   NewFeatures,
