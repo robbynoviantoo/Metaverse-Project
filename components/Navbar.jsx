72ecf855ece6efe32be6f3d9b102b73abcbb3137
Robby Novianto <heyitsrobby99@gmail.com>
2023-01-22 15:05:06 +0700

    prettier

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,9 +10,7 @@ const Navbar = () => (
     initial='hidden'
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}>
-    <div className='absolute w-[50%] inset-0 gradient-01' />
-
-
+    <div className='absolute w-[50%] inset-0 gradient-01'/>
       <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
         <img 
         src="/search.svg" 
