5eb4799b4df49dba72f503683087ea1f225b8189
Robby Novianto <heyitsrobby99@gmail.com>
2023-01-22 14:46:42 +0700

    hero

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 import styles from '../styles';
-import {navVariants} from '../utils/motion'
+import {navVariants} from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav
@@ -13,6 +13,8 @@ const Navbar = () => (
   >
     {/* Gradient Color */}
     <div className='absolute w-[50%] inset-0 gradient-01' />
+    {/*  */}
+
 
       <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
         <img 
@@ -23,6 +25,11 @@ const Navbar = () => (
         <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>
           METAVERSUS
         </h2>
+        <img 
+        src="/menu.svg" 
+        alt="menu"
+        className='w-[24px] h-[24px] object-contain'
+        />
       </div>
   </motion.nav>
 );
