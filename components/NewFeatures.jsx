99d4db2b057120ba6645b8efc114443debdd0239
Robby Novianto <heyitsrobby99@gmail.com>
2023-01-24 18:11:47 +0700

    What's new Section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,19 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from '../styles';
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
+      <img
+        src={imgUrl}
+        alt="icon"
+        className="w-1/2 h-1/2 object-contain"
+      />
+    </div>
+    <h1 className="mt-[24px] font-bold text-[24px] leading-[32px] text-white">{title}
+    </h1>
+    <p className="mt-[16px] font-normal text-[16px] leading-[29px] text-[#B0B0B0]">{subtitle}
+    </p>
+
   </div>
 );
 
--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,24 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="|  People on the World"
+        textStyles="text-center"
+      />
+
+    </motion.div>
   </section>
 );
 
